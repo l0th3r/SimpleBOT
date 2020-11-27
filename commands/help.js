ffd85ca40924320d933347624a90775da5cadcdb
L0th3r <alfabetatango9@gmail.com>
2020-11-27 23:46:39 +0100

    help command changed

--- commands/help.js
+++ commands/help.js
@@ -8,6 +8,6 @@ module.exports = {
         this.exemple = `${this.prefix}${this.name}`;
     },
     execute(message, args) {
-        message.react(':ok_hand:');
+        message.react('😄');
     }
 }
